ffef96fae156f338cc752bb09d6111dc98a4fc2d
ak4m410x01 <abdallah10kamal@gmail.com>
2024-04-28 08:01:58 +0300

    Use .AddTransient with IUnitOfWork

--- DoctorFinder.Persistence/Extensions/UnitOfWork/UnitOfWorkServiceCollection.cs
+++ DoctorFinder.Persistence/Extensions/UnitOfWork/UnitOfWorkServiceCollection.cs
@@ -13,7 +13,7 @@ namespace DoctorFinder.Persistence.Extensions.UnitOfWork
     {
         public static IServiceCollection AddUnitOfWork(this IServiceCollection services)
         {
-            services.AddSingleton<UnitOfWorkContract.IUnitOfWork, UnitOfWorkImplementation.UnitOfWork>();
+            services.AddTransient<UnitOfWorkContract.IUnitOfWork, UnitOfWorkImplementation.UnitOfWork>();
             return services;
         }
     }
