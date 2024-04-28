b875e1345ea348547bb044acbefd5e26fda77cd6
ak4m410x01 <abdallah10kamal@gmail.com>
2024-04-28 07:33:34 +0300

    Add Repository Pattern [IBaseRepository]

---
+++ DoctorFinder.Application/Contracts/Interfaces/Repositories/Base/IBaseRepository.cs
@@ -0,0 +1,47 @@
+﻿#region Using Directive Namespaces
+
+using System.Linq.Expressions;
+
+#endregion
+
+
+namespace DoctorFinder.Application.Contracts.Interfaces.Repositories.Base
+{
+    public interface IBaseRepository<T> where T : class
+    {
+        #region Get Objects List
+
+        Task<IQueryable<T>> GetAllAsync();
+        Task<IQueryable<T>> GetAllByAsync(Func<T, bool> predicate);
+        Task<IQueryable<TResult>> GetAllByAsync<TResult>(Func<T, bool> predicate, Func<T, TResult> selector);
+
+        #endregion
+
+
+        #region Get Object
+
+        Task<T?> GetByIdAsync(object id);
+        Task<T?> GetByAsync(Expression<Func<T, bool>> predicate);
+
+        #endregion
+
+
+        #region Add
+
+        Task AddAsync(T entity);
+
+        #endregion
+
+        #region Update
+
+        Task UpdateAsync(T entity);
+
+        #endregion
+
+        #region Remove
+
+        Task RemoveAsync(T entity);
+
+        #endregion
+    }
+}
---
+++ DoctorFinder.Persistence/Repositories/Base/BaseRepository.cs
@@ -0,0 +1,97 @@
+﻿#region Using Directive Namespaces
+
+using DoctorFinder.Application.Contracts.Interfaces.Repositories.Base;
+using DoctorFinder.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+#endregion
+
+namespace DoctorFinder.Persistence.Repositories.Base
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        #region Constructors
+
+        public BaseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        #endregion
+
+
+        #region Properties
+
+        protected ApplicationDbContext _context;
+
+        #endregion
+
+
+        #region Get Objects List
+
+        public async Task<IQueryable<T>> GetAllAsync()
+        {
+            return await Task.FromResult(_context.Set<T>().AsNoTracking());
+        }
+        public async Task<IQueryable<T>> GetAllByAsync(Func<T, bool> predicate)
+        {
+            return await Task.FromResult(_context.Set<T>().AsNoTracking().Where(predicate).AsQueryable());
+        }
+        public async Task<IQueryable<TResult>> GetAllByAsync<TResult>(Func<T, bool> predicate, Func<T, TResult> selector)
+        {
+            return await Task.FromResult(_context.Set<T>().AsNoTracking().Where(predicate).Select(selector).AsQueryable());
+        }
+
+        #endregion
+
+
+        #region Get Object
+
+        public async Task<T?> GetByIdAsync(object id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T?> GetByAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _context.Set<T>().AsNoTracking().FirstOrDefaultAsync(predicate);
+        }
+
+        #endregion
+
+
+        #region Add
+
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+
+            await _context.SaveChangesAsync();
+        }
+
+        #endregion
+
+        #region Update
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Set<T>().Update(entity);
+
+            await _context.SaveChangesAsync();
+        }
+
+        #endregion
+
+        #region Remove
+
+        public async Task RemoveAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+
+            await _context.SaveChangesAsync();
+        }
+
+        #endregion
+    }
+}
